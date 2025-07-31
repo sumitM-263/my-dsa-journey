833f34e743ebbfcbe9e276b003a53addca505d09
Sumit Maurya <hsumit263@gmail.com>
2025-07-31 21:56:36 +0530

    solution to Leetcode 739

---
+++ Stacks-and-Queues/daily-temperatures-739.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& temperatures) {
+        
+        int n = temperatures.size();
+
+        stack<int> st;
+
+        vector<int> ans(n,0);
+
+        for(int i=n-1;i>=0;i--){
+
+            while(!st.empty() && temperatures[st.top()]<=temperatures[i]){
+                st.pop();
+            }
+
+            ans[i] = st.empty()? 0 : st.top()-i;
+            st.push(i);
+        }
+
+        return ans;
+    }
+};
